fc659c44fd539d18f778f79243828f799d1c8044
marc7075 <marc7075@zbc.dk>
2022-03-08 14:35:46 +0100

    SQL code for Create Tables

--- DB/CreateDB.sql
+++ DB/CreateDB.sql
@@ -1,1 +1,17 @@
-﻿
+﻿CREATE TABLE Planets(
+	id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+	translation_id INT NOT NULL UNIQUE,
+	distance_from_sun INT NOT NULL,
+	diameter INT NOT NULL,
+	rotation_period DOUBLE PRECISION NOT NULL,
+	orbital_period DOUBLE PRECISION NOT NULL,
+	has_life BOOL NOT NULL
+);
+
+CREATE TABLE TranslatedPlanets(
+	id SERIAL NOT NULL UNIQUE,
+	lang VARCHAR(2) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	description_json TEXT NOT NULL,
+	PRIMARY KEY(id, lang)
+);
