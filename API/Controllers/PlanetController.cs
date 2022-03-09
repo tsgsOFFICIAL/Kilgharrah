92ba3764c8c047703c0d132c31819986aa143abb
Kristóf Neller <neller.kristof@outlook.com>
2022-03-09 13:09:34 +0100

    We just created the Models and added some comments to it.

---
+++ API/Controllers/PlanetController.cs
@@ -0,0 +1,45 @@
+﻿using API.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlanetController : ControllerBase
+    {
+        DBManager man = new DBManager();
+        // GET: api/<PlanetController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PlanetController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PlanetController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PlanetController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PlanetController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Models/Planet.cs
@@ -0,0 +1,35 @@
+﻿namespace API.Models
+{
+    public class Planet
+    {
+        /// <summary>
+        /// Id is the Id for the DataBase.
+        /// </summary>
+        public int id { get; private set; }
+        /// <summary>
+        /// Translation_Id is the foreign key for the Id of TranslatedPlanet.
+        /// </summary>
+        public int translation_id { get; private set; }
+        /// <summary>
+        /// Distance from the sun in KM.
+        /// </summary>
+        public int distance_from_sun { get; private set; }
+        /// <summary>
+        /// Diameter of the planets in KM.
+        /// </summary>
+        public int diameter { get; private set; }
+        /// <summary>
+        /// Rotation period is for a planet's time circling around it's own axis. 
+        /// </summary>
+        public double rotation_period { get; private set; }
+        /// <summary>
+        /// Orbital period is for a planet's time circling around another object, for ex.: the Sun.
+        /// </summary>
+        public double orbital_period { get; private set; }
+        /// <summary>
+        /// "Has life" is for where humans and(or) animals do live.
+        /// </summary>
+        public bool has_life { get; private set; } 
+        
+    }
+}
