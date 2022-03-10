7afcc9b68287858b5bce32b47c7c54a5f8639356
Kent Pedersen <webzone@kpdesign.dk>
2022-03-10 16:15:36 +0100

    Created first version og jsonifying the data in the console app

---
+++ JsonifyData/Planet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonifyData
+{
+    internal class Planet
+    {
+        /// <summary>
+        /// Language for the translation.
+        /// </summary>
+        public int Translation_id { get; set; }
+        /// <summary>
+        /// Name of the planet.
+        /// </summary>
+        public string? Name { get; set; }
+        /// <summary>
+        /// Distance from the sun in KM.
+        /// </summary>
+        public long Distance_from_sun { get; set; }
+        /// <summary>
+        /// Diameter of the planets in KM.
+        /// </summary>
+        public int Diameter { get; set; }
+        /// <summary>
+        /// Age of the planets in Years.
+        /// </summary>
+        public long Age { get; set; }
+        /// <summary>
+        /// Rotation period is for a planet's time circling around it's own axis. 
+        /// </summary>
+        public int Rotation_period { get; set; }
+        /// <summary>
+        /// Rotation speed in km/h. 
+        /// </summary>
+        public double Rotation_speed { get; set; }
+        /// <summary>
+        /// Orbital period is for a planet's time circling around another object, for ex.: the Sun.
+        /// </summary>
+        public double Orbital_period { get; set; }
+        /// <summary>
+        /// Amount of moons.
+        /// </summary>
+        public int Moons { get; set; }
+        /// <summary>
+        /// The planets mass.
+        /// </summary>
+        public string? Mass { get; set; }
+        /// <summary>
+        /// Highest messured temperature.
+        /// </summary>
+        public double Highest_temp { get; set; }
+        /// <summary>
+        /// Lowest messured temperature.
+        /// </summary>
+        public double Lowest_temp { get; set; }
+        /// <summary>
+        /// List of statements and facts of the planet.
+        /// </summary>
+        public List<string> Statements { get; set; }
+        /// <summary>
+        /// List of object with questions and answers to the planet.
+        /// </summary>
+        public List<QuestionAndAnswer> Quests { get; set; }
+
+      public Planet()
+        {
+            this.Statements = new List<string>();
+            this.Quests = new List<QuestionAndAnswer>();
+        }
+    }
+}
--- JsonifyData/Program.cs
+++ JsonifyData/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Kent!");
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.Json;
+using JsonifyData;
+
+
+
+
+
+
+
+
+List<Planet> planets = new List<Planet>()
+        {
+            new Planet
+            {
+                Translation_id = 1,
+                Name="Earth",
+                Distance_from_sun = 150000000,
+                Diameter= 12756,
+                Age=4500000000,
+                Rotation_period=1440,
+                Rotation_speed=1670,
+                Orbital_period=365,
+                Moons=1,
+                Mass="5972 * 1024 kg",
+                Highest_temp=56.7,
+                Lowest_temp=-89.2,
+                Statements={
+                    "Earth only has one moon.",
+                    "Earth is more than 4.5 billion years old.",
+                    "Earth takes a year to orbitate one round of the Sun.",
+                    "Earth rotates around its own axis in 24 hours.",
+                    "Earth is filled by: 71% of water, the rest are land.",
+                    "Humans are only on Earth (yet), and they emerged ca. 300 000 years ago. Nowadays, the human population is around 8 billion already.",
+                    "Every fourth year, we have to add an extra day, now containing 366 days, it's called &#34;Leap year&#34;.",
+                },
+                 Quests={
+                    new QuestionAndAnswer{
+                        Question="How many moons does Earth have? (3, 7, 1, 4)",
+                        Answers={ "3", "7", "1", "4" }
+                    },
+                    new QuestionAndAnswer{
+                        Question="How old is Earth? (100 thousand years, 6 million years, 4.5 billion years, 10 billion years)",
+                        Answers={ "100 thousand years", "6 million years", "4.5 billion years", "10 billion years" }
+                    },
+                    new QuestionAndAnswer{
+                        Question="What does the earth consist of?",
+                        Answers={ "71% water and 29% land", "29% water and 71% water", "50% water and 50% land", "85% water and 15% land" }
+                    },
+                    new QuestionAndAnswer{
+                        Question="How much time does the Earth need to rotate around its own axis?",
+                        Answers={ "12 hours", "6 hours", "24 hours", "48 hours" }
+                    }
+
+                }
+            },
+        };
+
+Console.WriteLine($"{JsonSerializer.Serialize(planets)}");
+Console.ReadKey();
+
+
