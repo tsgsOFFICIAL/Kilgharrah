ffd4e3618b8d8ab8c7a6212cdab28f2289fb9161
Marcus Jensen <marcusproman0507@gmail.com>
2022-03-13 18:58:07 +0100

    Privated http client

--- WWW/DAL/APIManager.cs
+++ WWW/DAL/APIManager.cs
@@ -5,11 +5,14 @@ namespace WWW.DAL
 {
     public class APIManager
     {
-        HttpClient client = new HttpClient();
+        /// <summary>
+        /// The HttpClient
+        /// </summary>
+        private readonly HttpClient _client = new HttpClient();
 
         public async Task<List<Planet>> GetPlanets()
         {
-            string json = await client.GetStringAsync("10.108.149.14/api/planets/");
+            string json = await _client.GetStringAsync("10.108.149.14/api/planets/");
             
             List<Planet>? planets = JsonSerializer.Deserialize<List<Planet>>(json);
 
