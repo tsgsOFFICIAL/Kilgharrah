886eb98a065afd99ea3a519b0c071b55935b10dd
Marcus Jensen <marcusproman0507@gmail.com>
2022-03-16 22:37:44 +0100

    Added facts, language control & abillity to change facts

--- Website/wwwroot/js/app.js
+++ Website/wwwroot/js/app.js
@@ -3,10 +3,17 @@ const textToTranslate = document.querySelectorAll("[data-translation]"); // An a
 const translations = JSON.parse(document.currentScript.getAttribute('translation_text')); // The translation text as JSON
 const planetObjects = JSON.parse(document.currentScript.getAttribute('planets_text')); // All planets & their information as JSON
 const planets = document.querySelectorAll("[data-click]"); // An array of all Planets
-const ws = new WebSocket("ws://10.108.149.14:8008"); // A WebSocket instance
 const modal = document.getElementById("information-modal"); // A modal, for displaying information about the planets
 const modal_close_btn = document.getElementById("information-modal-close-btn"); // The closing button 'x' for the modal
+let ws;
+try {
+    ws = new WebSocket("ws://10.108.149.14:8008"); // A WebSocket instance
+} catch (e) { }
 let lang; // The language code
+
+planetObjects.forEach(planetObj => {
+    planetObj.Info_as_json = JSON.parse(planetObj.Info_as_json);
+});
 
 // Find the language from the browser default.
 switch (navigator.language) {
@@ -28,7 +35,7 @@ switch (navigator.language) {
 }
 
 // A function to update the HTML with a new language, in case it was changed
-function UpdateLanguage(lang) {
+function UpdateLanguage() {
     textToTranslate.forEach(el => {
         switch (lang) {
             case 'da':
@@ -65,7 +72,8 @@ function UpdateLanguage(lang) {
 
 // A helper function to set the language, highlight the selected flag & update the HTML text
 function SetLanguage(el) {
-    UpdateLanguage(el.getAttribute("data-flag"));
+    lang = el.getAttribute("data-flag");
+    UpdateLanguage();
 
     flags.forEach(flag => {
         flag.classList.remove("selected");
@@ -87,7 +95,26 @@ flags.forEach(flag => {
 
 // Add an eventlistener of click to all planets
 planets.forEach(planet => {
+    /*
+    let planetIndex = planet.getAttribute("data-index");
+    let factIndex = 0;
+    let maxIndex = planetObjects[planetIndex].Info_as_json.Statements.length - 1;
+    */
     planet.addEventListener("click", () => {
+        let planetIndex = planet.getAttribute("data-index");
+
+        let index = 0;
+        for (let i = 0; i < planetObjects.length; i++) {
+            if (planetObjects[i].Id == planetIndex && planetObjects[i].Lang == lang) {
+                index = i;
+                i = planetObjects.length;
+            }
+        }
+        planetIndex = index;
+
+        let factIndex = 0;
+        let maxIndex = planetObjects[planetIndex].Info_as_json.Statements.length - 1;
+
         document.querySelector("body").style.overflowY = "hidden"; // Disable scroll on the body
         modal.classList.add("open");
 
@@ -97,8 +124,21 @@ planets.forEach(planet => {
         modalHeader.querySelector(".icon").src = planet.src;
         modalHeader.querySelector("span").innerText = planet.alt;
 
-        const modalContent = modal.querySelector(".content");
+        const modalContent = modal.querySelector(".content>p.fact");
+        const prevFact = modal.querySelector(".content .prev-btn");
+        const nextFact = modal.querySelector(".content .next-btn");
 
+        prevFact.addEventListener("click", () => {
+            factIndex = factIndex > 0 ? factIndex - 1 : 0;
+            modalContent.innerHTML = planetObjects[planetIndex].Info_as_json.Statements[factIndex];
+        });
+
+        nextFact.addEventListener("click", () => {
+            factIndex = factIndex < maxIndex ? factIndex + 1 : maxIndex;
+            modalContent.innerHTML = planetObjects[planetIndex].Info_as_json.Statements[factIndex];
+        });
+
+        modalContent.innerHTML = planetObjects[planetIndex].Info_as_json.Statements[factIndex]; // Set the fact to the first one
 
 
         // ws.send(planet.getAttribute("data-click")); // Send a WebSocket message to move an Arduino
